44a6983856183ce61f80018539fd4486b7605555
Darren322 <xiia0_darren@hotmail.com>
2022-02-12 22:38:37 +0800

    Update userCommands.py

--- commandsfunc/userCommands.py
+++ commandsfunc/userCommands.py
@@ -101,7 +101,7 @@ async def getrole(user: discord.User, bcGuild: discord.Guild, channel: discord.T
         trustedname = 'Trusted 5'
         return True, trustedname
 
-    elif trusted_role4 not in user.roles and trusted_role5 not in user.roles and len(us.vouches) >= 150:
+    elif trusted_role4 not in user.roles and trusted_role5 not in user.roles and len(us.vouches) >= 200:
         await user.add_roles(trusted_role4)
         await user.remove_roles(trusted_role3)
         await user.remove_roles(trusted_role2)
@@ -110,7 +110,7 @@ async def getrole(user: discord.User, bcGuild: discord.Guild, channel: discord.T
         return True, trustedname
 
 
-    elif trusted_role3 not in user.roles and trusted_role4 not in user.roles and trusted_role5 not in user.roles and len(us.vouches) >= 100:
+    elif trusted_role3 not in user.roles and trusted_role4 not in user.roles and trusted_role5 not in user.roles and len(us.vouches) >= 150:
         await user.add_roles(trusted_role3)
         await user.remove_roles(trusted_role2)
         await user.remove_roles(trusted_role)
@@ -118,14 +118,14 @@ async def getrole(user: discord.User, bcGuild: discord.Guild, channel: discord.T
         return True, trustedname
 
 
-    elif trusted_role2 not in user.roles and trusted_role3 not in user.roles and trusted_role4 not in user.roles and trusted_role5 not in user.roles and len(us.vouches) >= 60:
+    elif trusted_role2 not in user.roles and trusted_role3 not in user.roles and trusted_role4 not in user.roles and trusted_role5 not in user.roles and len(us.vouches) >= 100:
         await user.add_roles(trusted_role2)
         await user.remove_roles(trusted_role)
         trustedname = 'Trusted 2'
         return True, trustedname
        
 
-    elif trusted_role not in user.roles and trusted_role2 not in user.roles and len(us.vouches) >= 30:
+    elif trusted_role not in user.roles and trusted_role2 not in user.roles and len(us.vouches) >= 50:
         await user.add_roles(trusted_role)
         trustedname = 'Trusted 1'
         return True, trustedname
