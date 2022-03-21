6c33857094a62c65b2f8499c37d2aba045a4b657
Darren322 <xiia0_darren@hotmail.com>
2022-03-21 16:02:35 +0800

    Update userCommands.py

--- commandsfunc/userCommands.py
+++ commandsfunc/userCommands.py
@@ -56,7 +56,7 @@ async def vouch(user: discord.User,
 
 
     # Send embeds to the user
-    embed = newEmbed(description='', title=f'Vouch ID: {vouchNum}', timestamp = datetime.utcnow())
+    embed = newEmbed(description='', title=f'Vouch ID: {vouchNum}')
     embed.add_field(name='Receiver', value=f'<@!{target}>', inline=False)
     embed.add_field(name='Giver', value=f'<@!{giverr}>', inline=False)
     embed.add_field(name='Comment', value=message, inline=False)
@@ -69,7 +69,7 @@ async def vouch(user: discord.User,
         await user.send(embed=embed)
 
     # Send embed to log channel
-    embed = newEmbed(description='', title=f'Vouch ID: {vouchNum}', timestamp = datetime.utcnow())
+    embed = newEmbed(description='', title=f'Vouch ID: {vouchNum}')
     embed.add_field(name='Receiver', value=f'<@!{target}>', inline=False)
     embed.add_field(name='Giver', value=f'<@!{giverr}>', inline=False)
     embed.add_field(name='Comment', value=message, inline=False)
